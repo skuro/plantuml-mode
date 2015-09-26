f47a0c559a56c946daa92abc24091237b5112c9e
Carlo Sciolla <skuro@skuro.tk>
2015-09-26 11:22:45 +0200

    Prepare for bugfix release

--- puml-mode.el
+++ puml-mode.el
@@ -27,6 +27,7 @@
 
 ;;; Change log:
 ;;
+;; version 0.6.1, 2015-09-26 Bugfix: use eq to compare symbols instead of cl-equalp
 ;; version 0.6, 2015-09-26 Fixed PNG preview
 ;; version 0.5, 2015-09-21 Added preview capabilities
 ;; version 0.4, 2015-06-14 Use a puml- prefix to distinguish from the other plantuml-mode
