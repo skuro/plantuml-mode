4f8e937dc25fcec56912b029660d091ab28e6bb7
Carlo Sciolla <carlo.sciolla@gmail.com>
2016-11-11 11:51:49 +0100

    Added Cask and ert-runner infrastructure

---
+++ test/plantuml-mode-test.el
@@ -0,0 +1,15 @@
+;;; plantuml-mode-test.el --- PlantUML Mode tests   -*- lexical-binding: t; -*-
+
+;; Author: Carlo Sciolla (skuro)
+;; Maintainer: Carlo Sciolla (skuro)
+;; URL: https://github.com/skuro/plantuml-mode
+
+;;; Commentary:
+
+;;; Code:
+
+(ert-deftest initial-jar-location ()
+  (should (equal (expand-file-name "~/plantuml.jar")
+                 plantuml-jar-path)))
+
+;;; plantuml-mode-test.el ends here
