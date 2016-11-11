8752e703defa96d550a986261523c9a22f209a7b
Carlo Sciolla <carlo.sciolla@gmail.com>
2016-11-11 12:53:41 +0100

    Working test setup which also tests preview

--- test/test-helper.el
+++ test/test-helper.el
@@ -16,6 +16,22 @@
 (defvar package-code-path
   (f-parent package-test-path))
 
+(defvar plantuml-test-resources-path
+  (f-join package-code-path "test/resources"))
+
+(defvar plantuml-test-jar-path
+  (f-join package-code-path "bin/plantuml.jar"))
+
+(defun read-buffer (bufname)
+  (with-current-buffer (get-buffer bufname)
+    (buffer-string)))
+
+(defun read-preview-buffer ()
+  (read-buffer plantuml-preview-buffer))
+
+(defun read-test-file (path)
+  (f-read (f-join plantuml-test-resources-path path) 'utf-8))
+
 (require 'plantuml-mode (f-expand "plantuml-mode.el" package-code-path))
 
 ;;; test-helper.el ends here
