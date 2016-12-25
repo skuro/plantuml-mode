f8148e9d5f9b581d150dae1209b13448c94940dd
Carlo Sciolla <carlo.sciolla@gmail.com>
2016-12-25 18:29:48 +0100

    #50 Factored out preview assertion function

--- test/plantuml-preview-test.el
+++ test/plantuml-preview-test.el
@@ -8,12 +8,15 @@
 
 ;;; Code:
 
+(defun assert-preview (puml output)
+  (plantuml-preview-string 42 (read-test-file puml))
+  (sleep-for 2)
+  (should (equal (read-test-file output) (read-preview-buffer))))
+
 (ert-deftest preview-test ()
   (setq-local plantuml-jar-path plantuml-test-jar-path)
   (setq-local plantuml-output-type "utxt")
-  (plantuml-preview-string 42 (read-test-file "a-b.puml"))
-  (sleep-for 2)
-  (should (equal (read-test-file "a-b.txt") (read-preview-buffer))))
+  (assert-preview "a-b.puml" "a-b.txt"))
 
 (provide 'plantuml-mode-preview-test)
 
