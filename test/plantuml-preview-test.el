343bc5a753067ad25d0c9d114b41e4dfdc593195
Carlo Sciolla <carlo.sciolla@gmail.com>
2016-12-25 20:28:54 +0100

    #50 use different tests for different outputs

--- test/plantuml-preview-test.el
+++ test/plantuml-preview-test.el
@@ -17,14 +17,10 @@
 
 (ert-deftest preview-utxt-test ()
   (setq-local plantuml-jar-path plantuml-test-jar-path)
-  (setq-local plantuml-output-type "utxt")
-
   (assert-preview "a-b.puml" "a-b.txt"))
 
 (ert-deftest preview-unicode-test ()
   (setq-local plantuml-jar-path plantuml-test-jar-path)
-  (setq-local plantuml-output-type "utxt")
-
   (assert-preview "unicode.puml" "unicode.txt"))
 
 (provide 'plantuml-mode-preview-test)
