ecb4428218cab08db650a64c5c7c205fa3935a67
Carlo Sciolla <carlo.sciolla@gmail.com>
2017-08-18 12:16:12 +0200

    #60 Upgrading license to GPLv3+

--- plantuml-mode.el
+++ plantuml-mode.el
@@ -9,10 +9,18 @@
 ;; Version: 1.2.3
 ;; Package-Requires: ((emacs "24"))
 
-;; You can redistribute this program and/or modify it under the terms
-;; of the GNU General Public License as published by the Free Software
-;; Foundation; either version 2
-;; NOTE: licensing fixed to GPLv2 as per original author comment
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
